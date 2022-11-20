8bdc99fe8b153e1c592f9710352542f6f0f726b8
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2022-11-20 12:31:51 -0500

    ok

---
+++ src/main/java/org/example/Jugador.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Jugador {
+
+    String nombre="James";
+    int edad;
+    double apellidos;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+}
