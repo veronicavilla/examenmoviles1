1335b6f7273184df0914cf8d86ca88c29f969ceb
308 <308@academicos.cesde.edu.co>
2022-11-24 11:36:09 -0500

    Examen terminado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,7 +20,6 @@ public class Main {
         do {
 
 
-
             System.out.println("Menu de jugador");
             System.out.println("+++++++++++++++");
             System.out.println("1. Agregar jugador");
@@ -33,7 +32,7 @@ public class Main {
             switch (variableDeControl) {
 
                 case 1:
-                    if (jugadores.size() < 23) {
+                    if (jugadores.size() < 3) {
                         Jugador objetoJugador = new Jugador();
                         System.out.print("Ingrese el nombre del jugador: ");
                         objetoJugador.setNombre(entradaPorTeclado.next());
@@ -45,20 +44,62 @@ public class Main {
                         objetoJugador.setPosicion(entradaPorTeclado.next());
                         System.out.print("Ingrese edad del jugador: ");
                         objetoJugador.setEdad(entradaPorTeclado.nextInt());
+                        System.out.print("Ingrese equipo del jugador: ");
+                        objetoJugador.setEquipoDondeJuega(entradaPorTeclado.next());
+                        jugadores.add(objetoJugador);
+                    } else {
+                        System.out.println("Llego al limite, no permitido");
                     }
                     break;
                 case 2:
-
+                    System.out.println("Digite el numero de la camiseta del jugador a consultar: ");
+                    int buscarJugador = entradaPorTeclado.nextInt();
+                    boolean jugadorEncontrado = false;
+                    for (Jugador jugador : jugadores) {
+                        if (buscarJugador == jugador.getNumeroCamiseta()) {
+                            System.out.println("numero de camiseta del jugador: " + jugador.getNumeroCamiseta());
+                            System.out.println("nombre del jugador: " + jugador.getNombre());
+                            System.out.println("apellido del jugador: " + jugador.getApellidos());
+                            System.out.println("edad del jugador: " + jugador.getEdad());
+                            System.out.println("posicion del jugador: " + jugador.getPosicion());
+                            System.out.println("equipo del jugador: " + jugador.getEquipoDondeJuega());
+                            jugadorEncontrado = true;
+                        }
+                    }
+                    if (!jugadorEncontrado) {
+                        System.out.println("Jugador no encontrado.");
+                    }
                     break;
                 case 3:
-
+                    System.out.println("Editar jugador");
+                    System.out.println("Ingrese el numero de camisa del jugador a editar: ");
+                    int editarJugador = entradaPorTeclado.nextInt();
+                    jugadorEncontrado = false;
+                    for (Jugador jugador : jugadores) {
+                        if (editarJugador == jugador.getNumeroCamiseta()) {
+                            System.out.println("editar nombre: ");
+                            jugador.setNombre(entradaPorTeclado.next());
+                            jugadorEncontrado = true;
+                        }
+                    }
+                    if (!jugadorEncontrado) {
+                        System.out.println("Jugador no encontrado.");
+                    }
                     break;
 
                 case 4:
-
+                    for (Jugador jugador : jugadores) {
+                        System.out.println("numero de camiseta del jugador: " + jugador.getNumeroCamiseta());
+                        System.out.println("nombre del jugador: " + jugador.getNombre());
+                        System.out.println("apellido del jugador: " + jugador.getApellidos());
+                        System.out.println("edad del jugador: " + jugador.getEdad());
+                        System.out.println("posicion del jugador: " + jugador.getPosicion());
+                        System.out.println("equipo del jugador: " + jugador.getEquipoDondeJuega());
+                    }
+                    break;
 
                 default:
-                    System.out.println("Selecciona una opcion valida");
+                    System.out.println("Convocatoria completa");
             }
         } while (variableDeControl != 5);
     }
